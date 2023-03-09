a36bb824e6e61b3311b5c6b92f2ae5d8a15d9bd7
yeonsu65 <daum3659@gmail.com>
2023-03-09 10:08:15 +0900

    first commit

--- js/main.js
+++ js/main.js
@@ -1,24 +1,19 @@
-window.addEventListener('load', () =>{
-  // 배치할 요소를 감싸고 있는 부모 요소명
-  const grid = new Isotope('section',{
-    itemSelector:'article', //배치할 요소
-    columnWidth: 'article', //너비값을 구할 요소명
-    transitionDuration:'0.5s' // 화면 재배치 시 요소가 움직이는 시간
-  });
 
-  const btns =document.querySelectorAll('main ul li');
-  for(let el of btns){
-    // 각 버튼에 클릭 이벤트 연결
-    el.addEventListener('click', (e) =>{
-      e.preventDefault();
-      // 변수 sort에 클릭한 대상의 자식인 a요소의 href 속성값저장
-      const sort = e.currentTarget.querySelector('a').getAttribute('href');
-      console.log(sort);
+// slide menu
+$('span.select_btn').click(function(){
+  $('ul.select').stop().slideDown();
+  $('span.select_btn').removeClass('off');
+  $(this).addClass('off');
+});
+$('span.select_btn.off').click(function(){
+  $('ul.select').stop().slideUp();
+});
 
-      // grid에 결과값으로 재정렬 기능
-      grid.arrange({
-        filter : sort
-      });
-    });
-  }
+$('button.menu_btn').click(function(){
+  $('header div.inner-wrap.on').stop().slideDown();
+  $('button.menu_btn').removeClass('on');
+  $(this).addClass('on');
+});
+$('button.menu_btn.on').click(function(){
+  $('header div.inner-wrap.on').stop().slideUp();
 });
